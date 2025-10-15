6e2f8506cc653eb0ebe89eaba88c475ffa6aa01b
Khánh Duy Bùi <cqbtcqbnvl@gmail.com>
2025-10-16 00:27:07 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['res.cloudinary.com', 'images.unsplash.com', 'ui-avatars.com']
+  },
+  // Disable PWA for deployment testing
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client']
   }
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
